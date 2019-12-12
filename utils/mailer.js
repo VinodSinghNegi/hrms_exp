ae802745efed3a5f07a7dca2c598d62c199b637c
VinodSinghNegi <57259776+VinodSinghNegi@users.noreply.github.com>
2019-12-12 13:52:13 +0530

    Add files via upload

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,36 +1,35 @@
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
-module.exports = function (email,verify){
-console.log(email,verify);
+module.exports = function(email, verify) {
+  console.log(email, verify);
 
-var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-host:"mail.vinove.com" ,
-secure:false,
-  port:587,
-  auth: {
-    user: 'milan.srivastava@mail.vinove.com',
-    pass: 'milan@2019'
-  },
-  tls:{
-      rejectUnauthorized:false
-  }
-});
-const message1=`You requested for a password reset, kindly use this ${verify} to reset your password`
+  var transporter = nodemailer.createTransport({
+    //   service: 'gmail',
+    host: "mail.vinove.com",
+    secure: false,
+    port: 587,
+    auth: {
+      user: "milan.srivastava@mail.vinove.com",
+      pass: "milan@2019"
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+  const message1 = `You requested for a password reset, kindly use this ${verify} to reset your password`;
 
+  var mailOptions = {
+    from: "milan.srivastava@mail.vinove.com",
+    to: email,
+    subject: "Sending Email using Node.js",
+    text: `Please click this link ${verify} to approve your login with email:"your email id" , password:"name+yearofjoining" . example-> email: abc.gmail.com , password: abc2019 `
+  };
 
-var mailOptions = {
-  from: 'milan.srivastava@mail.vinove.com',
-  to:email,
-  subject: 'Sending Email using Node.js',
-  text:`Please click this link ${verify} to approve your login  `
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 };
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-})
-}
